dfbc6001d0d43ccfedd642bf55704b01534e74ad
naoki <pika1675@yahoo.co.jp>
2018-07-12 20:22:46 +0900

    fix some

--- postWP.js
+++ postWP.js
@@ -1,16 +1,5 @@
 /*
-tegaki.jsで入力選択されたものをpostするjs
-
-1.DB->WPには認識文字のDBを作成する.
-1文字が候補で2文字以上を選択した場合は1文字に対して2文字をDBに
-2文字以上であれば,1文字と対象文字に対してDBに
-
-*DBの優先度を考えて,jsのが配列弄りやすそうなのでこっちで調整する.
-getで常にDBから予測候補は取ってあるのでそれを弄って送ることにする
-(だから,suggestでは極力配列を弄らない方向で...)
-
-2.文字を選んだ後の候補,DB->TPのDBを作成する.
-次の文字が入力されるまで入力文字を保持しておいて,繋がるようにDBを作成
+tegaki.jsで入力選択されたものをpost
 */
 
 function postDB_WP(send_data){
@@ -22,22 +11,16 @@ function postDB_WP(send_data){
 		dataType: 	'json',
 		data: send_data,
 		success: function(){	// 通信に成功した場合の処理
-
 		},
 		complete: function() {	// 通信を終了した場合の処理（成功・失敗問わず）
-
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {	// 通信に失敗した場合の処理
-
 			//alert('\n通信に失敗しました。\nXMLHttpRequest : ' + XMLHttpRequest.status + '\ntextStatus : ' + textStatus + '\nerrorThrown : ' + errorThrown.message +'\n');
-
 		}
 	});
 }
 
-
 /* getした配列から辞書が複数になるように...(ry) */
-//よーするに辞書がダブらないように突っ込むためのなんか
 function post_WP(key,word,flg,Prediction_obj){
   postDB_WP(checkDB_WP(key.slice(0,1),word,flg,Prediction_obj));
 }
